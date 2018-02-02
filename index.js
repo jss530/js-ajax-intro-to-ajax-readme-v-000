e647d4b3852a1656912dbd5e7a707f9a00ec959e
jss530 <jsifre01@gmail.com>
2018-02-02 16:58:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
-function loadRepositories() {
-   const req = new XMLHttpRequest();
-   req.addEventListener('load', showRepositories)
-   req.open('GET', 'https://api.github.com/users/yehudamakarov/repos')
-   req.send()
- }
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
 
- function showRepositories() {
-   const arrOfRepos = JSON.parse(this.responseText)
-   let repoList = '<ul>'
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
 
-   for (var i = 0; i < arrOfRepos.length; i++) {
-     repoList += '<li>' + arrOfRepos[i].name + ' - <a href="#" data-repo="' + arrOfRepos[i].name +  '" onclick="getCommits(this)">Get Commits</a></li>';
-   }
-
-   repoList += '</ul>'
-
-   document.getElementById('repositories').innerHTML = repoList
- }
-
- function getCommits(linkEl) {
-   const name = linkEl.dataset.repo
-   const req = new XMLHttpRequest()
-   req.addEventListener('load', showCommits)
-   req.open('GET', 'https://api.github.com/repos/yehudamakarov/' + name + '/commits')
-   req.send()
- }
-
- function showCommits() {
-   const commitsArray = JSON.parse(this.responseText)
-   let commitsHTML = '<ul>'
-
-
-   for (var i = 0; i < commitsArray.length; i++) {
-     if (commitsArray[i].author) {
-       commitsHTML += '<li>' + commitsArray[i].author.login + ' - Meaasage: ' + commitsArray[i].commit.message + '</li>'
-     } else {
-       commitsHTML += '<li>---Anonymous - Meaasage: ' + commitsArray[i].commit.message + '</li>'
-     }
-
-   }
-
-   commitsHTML += '</ul>'
-
-   document.getElementById('commits').innerHTML = commitsHTML
- }
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
